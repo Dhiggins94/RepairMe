b1591938a878d59d7147a1448de67494f0d0027b
Divine Higgins <dhiggins94@gmail.com>
2021-08-27 16:22:48 -0400

    added redirects, still needed to work on frontend

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'deployed link goes here'
+    ? 'https://repairme-api.herokuapp.com/'
     : 'http://localhost:3000';
 
 const api = axios.create({
