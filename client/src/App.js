e817f49147f1edaf448d423819e03d72405e7270
Divine Higgins <dhiggins94@gmail.com>
2021-08-28 20:22:07 -0400

    working on screens and maincontainter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,47 @@
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import './App.css';
+import MainContainer from './containers/MainContainer';
+import Layout from './layouts/Layout';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import {
+  loginUser,
+  registerUser,
+  verifyUser,
+  removeToken,
+} from './services/auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+  
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+    history.push('/');
+  };
   return (
     <div className="App">
       
---
+++ client/src/screens/RepairGuideDetail.jsx
@@ -0,0 +1,36 @@
+// import { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { getOneRepairGuide } from '../services/repairGuide';
+
+// export default function RepairGuideDetail(props) {
+
+//   const [guides, setGuides] = useState(null);
+//   const { id } = useParams();
+//   const {electronics} = props
+
+//   useEffect(() => {
+//     const fetchRepairGuide = async () => {
+//       const guideData = await getOneRepairGuide(id)
+//       setGuides(guideData)
+//     }
+//     fetchRepairGuide();
+//   }, [id]);
+
+//   return (
+//     <div>
+//       <h3>{guides?.title }</h3>
+//       {guides?.electronics.map((electronics) => {
+//         <p key={electronic.id}>{electronic.id}</p>
+//       })}
+//       <form>
+
+//       </form>
+//     </div>
+//   )}
+
+ {/* <h2>{electronics.title}</h2>
+      
+      <br />
+      <img src={electronics?.image} alt="device"></img>
+      <br />
+      <p>{electronics?.steps}</p> */}
