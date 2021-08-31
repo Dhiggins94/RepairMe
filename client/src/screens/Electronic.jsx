4df1795bf875e50949dbb7a304f8fe4770061733
Divine Higgins <dhiggins94@gmail.com>
2021-08-31 17:17:30 -0400

    working through yellow errors, mvp acheived

--- client/src/screens/Electronic.jsx
+++ client/src/screens/Electronic.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Electronic.css"
-
+import { useParams } from "react-router";
 export default function Electronic(props) {
+  const {id} = useParams()
+
   const { electronics } = props;
   return (
     <div>
@@ -9,7 +11,7 @@ export default function Electronic(props) {
       {electronics.map((electronic) => (
         <div>
           <p className="e-text" key={electronic?.id}>{electronic?.name}</p>
-          <a href='/electronics/${id}'>
+          <a href={`/electronics/${id}`}>
             <img src={electronic?.image_url} alt="images" className="e-image"></img>
           </a>
           <p className="e-description">{electronic?.description}</p>
