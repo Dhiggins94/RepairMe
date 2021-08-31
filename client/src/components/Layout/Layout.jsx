0b4ec5fe9071ac3d42000fa0695f0f537a46c889
Divine Higgins <dhiggins94@gmail.com>
2021-08-31 14:09:47 -0400

    added media and flexboxx css

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -8,17 +8,17 @@ export default function Layout(props) {
       {currentUser ? (
         <div>
           <h2 className="username"> Welcome! {currentUser.username} </h2>
-          <button onClick={handleLogout}>Logout</button>
+          <button className="logout" onClick={handleLogout}>Logout</button>
         </div>
       ) : (
-        <Link to='/login'>Login/Register</Link>
+        <Link className="login-register"to='/login'>Login/Register</Link>
       )}
       <hr />
       {currentUser && (
         <div>
-          <Link to='/electronics'>Electronics</Link>
+          <Link className="electronics"to='/electronics'>Electronics</Link>
           <br />
-          <Link to='/repair_guides'>Repair Guides</Link>
+          <Link className="repair-guides"to='/repair_guides'>Repair Guides</Link>
         </div>
       )}
       {props.children}
