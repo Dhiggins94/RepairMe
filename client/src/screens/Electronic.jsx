f23768a8c087ee193897b19b0fff9b5b929e32d5
Divine Higgins <dhiggins94@gmail.com>
2021-08-31 21:23:21 -0400

    working on deloyment and media queries

--- client/src/screens/Electronic.jsx
+++ client/src/screens/Electronic.jsx
@@ -9,7 +9,7 @@ export default function Electronic(props) {
     <div>
       <h2> List of all Electronics</h2>
       {electronics.map((electronic) => (
-        <div>
+        <div key={electronic?.id}>
           <p className="e-text" key={electronic?.id}>{electronic?.name}</p>
           <a href={`/electronics/${id}`}>
             <img src={electronic?.image_url} alt="images" className="e-image"></img>
